7999dea766ea53d1c27a32f1491588af58f871eb
Alois Klink <alois.klink@gmail.com>
2016-12-10 01:03:58 +0000

    Fixes possible bugs inside the script

--- TeXStudioSetup.sh
+++ TeXStudioSetup.sh
@@ -9,15 +9,15 @@ sudo apt-get autoremove #texlive-full contains some useless stuff
 # can be added to TeXStudio using
 #     `Options`->`Configure TeXStudio`->`Language Checking`->`Spell Check`
 mkdir dictionaries/
-cd dictionaries/
+cd dictionaries/ || exit
 mkdir tmp/
-cd tmp/
+cd tmp/ || exit
 wget https://extensions.libreoffice.org/extensions/english-dictionaries/2016.09.01/@@download/file/dict-en.oxt 
 unzip dict-en.oxt
 mv en_US.* ../
-cd ../
+cd ../ || exit
 rm -rf tmp/
-cd ../
+cd ../ || exit
 
 # downloads Grammar Checker for TeXStudio
 # add it to TeXStudio by going to:
@@ -30,11 +30,11 @@ rm -rf tmp/
 # installs IEEE bibliograpgy style for BibTeX
 # Can be used by placing `\bibliographystyle{IEEEtran}` into LaTeX file
 mkdir tmp/
-cd tmp/
+cd tmp/ || exit
 wget http://www.ieee.org/documents/IEEEtranBST.zip
 unzip IEEEtranBST.zip
 sudo mkdir /etc/texmf/bibtex/
 sudo mkdir /etc/texmf/bibtex/bst/
-sudo mv *.bst /etc/texmf/bibtex/bst/
-cd ../
+sudo mv ./*.bst /etc/texmf/bibtex/bst/
+cd ../ || exit
 rm -rf tmp/
--- installOpenAIGym.sh
+++ installOpenAIGym.sh
@@ -3,7 +3,7 @@
 # Currently, python, git, make, and gcc need to already be installed
 
 mkdir tmp/
-cd tmp/
+cd tmp/ || exit
 
 # checks if a commadExists (ie it is installed)
 commandExists () {
@@ -22,24 +22,24 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib
 #installs cmake
 if ! commandExists cmake; then
 	git clone https://cmake.org/cmake.git 
-	cd cmake
+	cd cmake/ || exit
 	./configure --prefix=~/.local/
 	make
 	make install
-	cd ../
+	cd ../ || exit
 else
 	#checks if cmake's shared library is the correct version
 	#if cmake is compiled on a newer computer, it might not run on an older computer
 	regex="(GLIBC_)(\d\.\d*)"
 	regex1="(\d\.\d*)"
 	LIBC_VER_NEEDED=ldd ~/.local/bin/cmake |& grep -Po regex | grep -Pom 1 regex1
-	LIBC_CURRENT_VER=ldd --version | awk '/ldd/{print $NF}'
+	LIBC_CURRENT_VER=$(ldd --version | awk '/ldd/{print $NF}')
 
-	if [ bc "$LIBC_VER_NEEDED > $LIBC_CURRENT_VER" ]; then
+	if bc "$LIBC_VER_NEEDED > $LIBC_CURRENT_VER" ; then
 	  #builds and compiles latest version of libc
 		git clone git://sourceware.org/git/glibc.git
 		mkdir glibcbuild/
-		cd glibcbuild/
+		cd glibcbuild/ || exit
 		../glibc/configure --prefix=~/.local/
 		make
 		make install
@@ -48,6 +48,6 @@ fi
 
 #installs openai
 git clone https://github.com/openai/gym.git
-cd gym
+cd gym/ || exit
 pip install -e '.[atari]' --user
-cd ../
+cd ../ || exit
