b60cc56b051a31964b542f097955256023b18ad4
ak9g14 <ak9g14@m13.ecslabs.soton.ac.uk>
2016-12-14 10:17:09 +0000

    Adds correct PATH System Variable to Script

--- installMaven.sh
+++ installMaven.sh
@@ -2,11 +2,13 @@
 # downloads, extracts, and installs Maven
 
 export JAVA_HOME=dirname $(dirname $(dirname $(readlink -f $(which java) )))
+export PATH=~/.local/bin/:$PATH
 
 mkdir tmp/
 cd tmp/ || exit
 wget http://apache.mirror.anlx.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 
 tar xzvf apache-maven-3.3.9-bin.tar.gz
+mkdir ~/.local/
+mv apache-maven-3.3.9/* ~/.local/
 
-export PATH=$PATH:"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"tmp/apache-maven-3.3.9/bin/
